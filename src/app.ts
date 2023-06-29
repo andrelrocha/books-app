1b4e0dc364f083c4baf4ec37030ebbfaac608dd2
andrelrocha <andrerocha0911@gmail.com>
2023-06-29 16:20:31 -0300

    refactor: error handling in a separate dir to be called in app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,33 +5,16 @@ import { Request, Response, NextFunction } from "express";
 import { db } from "./db/dbConnect";
 
 import { router } from "./routes";
+import { errorHandle400 } from "./error/errorHandle400";
 
 
 db.on("error", console.log.bind(console, "Database connection error!"));
 db.once("open", () => console.log("Database connected!"));
 
 const app = express();
-
 app.use(express.json());
 
-
 app.use(router);
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
-    console.error(err);
-
-    if (typeof err === "string") {
-        return res.status(400).send(err);
-    }
-
-    if (err instanceof Error) {
-        // The error is an instance of the Error class, which has a toString() method
-        return res.status(400).send(err.toString());
-    }
-
-    return res.status(500).send("Internal Server Error");
-});
-
+app.use(errorHandle400);
 
 export { app };
